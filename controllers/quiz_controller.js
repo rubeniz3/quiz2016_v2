be0b1754439adedfd7d8a118bc356b99b44193ec
Ruben <ruben-i-f@hotmail.com>
2016-05-23 19:22:49 +0100

    Tema 10: Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,26 +1,36 @@
 var models = require('../models');
 
-// GET /question
-exports.question = function(req, res, next) {
-  models.Quiz.findOne().then(function(quiz) { // Busca la primera pregunta
+
+// GET /quizzes
+exports.index = function(req, res, next) {
+  models.Quiz.findAll().then(function(quizzes) {
+    res.render('quizzes/index.ejs', {quizzes: quizzes});
+  })
+  .catch(function(error) { next(error); });
+};
+
+
+// GET /quizzes/:id
+exports.show = function(req, res, next) {
+  models.Quiz.findById(req.params.quizId).then(function(quiz) {
     if (quiz) {
       var answer = req.query.answer || '';
-      res.render('quizzes/question', {question: quiz.question, answer: answer});
+      res.render('quizzes/show', {quiz: quiz, answer: answer});
     } else {
-        throw new Error('No hay preguntas en la BBDD.');
+        throw new Error('No existe ese quiz en la BBDD.');
     }
   }).catch(function(error) { next(error);});
 };
 
-//GET /check
-exports.check = function(req, res, next) {
-  models.Quiz.findOne().then(function(quiz) {  //Busca la primera pregunta
+//GET /quizzes/:id/check
+exports.check = function(req, res) {
+  models.Quiz.findById(req.params.quizId).then(function(quiz) {
     if (quiz) {
       var answer = req.query.answer || "";
       var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-      res.render('quizzes/result', {result: result, answer: answer});
+      res.render('quizzes/result', { quiz: quiz, result: result, answer: answer});
     } else {
-        throw new Error('No hay preguntas en la BBDD.');
+        throw new Error('No existe ese quiz en la BBDD.');
     }
   }).catch(function(error) { next(error); });
 };
