c2ef7caebe3d3441f8ea0a878054b231862cdd31
rubeniz3 <ruben-i-f@hotmail.com>
2016-05-24 14:28:17 +0100

    Practica 11: Formatos

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,8 @@ router.get('/author', function(req, res, next) {
 router.param('quizId', quizController.load); //autoload :quizId
 
 // Definición de rutas de /quizzes
-router.get('/quizzes',				quizController.index);
-router.get('/quizzes/:quizId(\\d+)',		quizController.show);
+router.get('/quizzes.:format?',				quizController.index);
+router.get('/quizzes/:quizId(\\d+).:format?',		quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check',	quizController.check);
 router.get('/quizzes/new',			quizController.new);
 router.post('/quizzes',				quizController.create);
