0bad037a6c2c2fc2b766776882a27904cf18cb8a
rubeniz3 <ruben-i-f@hotmail.com>
2016-05-24 12:43:20 +0100

    Tema 12: Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -35,9 +35,30 @@ exports.show = function(req, res, next) {
   res.render('quizzes/show', {quiz: req.quiz, answer: answer});
 };
 
-//GET /quizzes/:id/check
+// GET /quizzes/:id/check
 exports.check = function(req, res) {
   var answer = req.query.answer || ""; 
   var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
   res.render('quizzes/result', { quiz: req.quiz, result: result, answer: answer });
+};
+
+
+// GET /quizzes/new
+exports.new = function(req, res, next) {
+  var quiz = models.Quiz.build({question: "", answer: ""});
+  res.render('quizzes/new', {quiz: quiz});
+};
+
+
+// POST /quizzes/create
+exports.create = function(req,res,next) {
+  var quiz = models.Quiz.build({ question: req.body.quiz.question, answer: req.body.quiz.answer});
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["question", "answer"]}).then(function(quiz) {
+    res.redirect('/quizzes');
+  })
+  .catch(function(error) {
+    next(error);
+ });
 };
