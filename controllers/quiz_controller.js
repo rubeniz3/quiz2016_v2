fa7ed858dec6b3ad1b90518060185cdf1fa9028b
rubeniz3 <ruben-i-f@hotmail.com>
2016-05-25 18:19:47 +0100

    Tema 22: Autores de Quizzes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -61,10 +61,12 @@ exports.new = function(req, res, next) {
 
 // POST /quizzes/create
 exports.create = function(req,res,next) {
-  var quiz = models.Quiz.build({ question: req.body.quiz.question, answer: req.body.quiz.answer});
+  var authorId = req.session.user && req.session.user.id || 0;
+
+  var quiz = models.Quiz.build({ question: req.body.quiz.question, answer: req.body.quiz.answer, AuthorId: authorId});
 
 // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["question", "answer"]}).then(function(quiz) {
+  quiz.save({fields: ["question", "answer", "AuthorId"]}).then(function(quiz) {
     req.flash('success', 'Quiz creado con éxito.');
     res.redirect('/quizzes');  // res.redirect
   })				// Redirección HTTP a la lista de preguntas
